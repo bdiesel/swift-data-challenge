58b7c738e0257a01ed4807282af2db36ca40714d
Brian Diesel <bri@ndiesel.net>
2025-01-15 11:11:25 -0500

    Initial Commit

---
+++ swift-data-challenge/swift_data_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_challengeApp.swift
+//  swift-data-challenge
+//
+//  Created by Brian Diesel on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_data_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
