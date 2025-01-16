bf31e16f5dbd3e042f9e73ab679c0fe9314e469c
Brian Diesel <bri@ndiesel.net>
2025-01-16 12:10:19 -0500

    Loading user data from json

--- swift-data-challenge/ContentView.swift
+++ swift-data-challenge/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+struct User: Decodable {
+    let id: String
+    let name: String
+}
+
+struct Friend: Codable{
+    let id: String
+    let name: String
+}
+
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(users, id: \.id) { user in
+                    VStack(alignment: .leading) {
+                        Text(user.name)
+                    }
+                }
+            }.task {
+                await loadData()
+            }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
         }
-        .padding()
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            if let decodedResponse = try? JSONDecoder().decode([User].self, from: data) {
+                users = decodedResponse
+            } else {
+                print("Could not parse data")
+            }
+            // more code to come
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
